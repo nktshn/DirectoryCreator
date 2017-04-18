0060acf78947ca0e19cf8c569007343ef61a77cc
Shishin Nikita <nktshn06@gmail.com>
2017-04-18 16:27:40 +0600

    release

---
+++ DirectoryCreator/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DirectoryCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.createButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(314, 174);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // createButton
+            // 
+            this.createButton.Enabled = false;
+            this.createButton.Location = new System.Drawing.Point(250, 221);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(75, 23);
+            this.createButton.TabIndex = 1;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(13, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Enabled = false;
+            this.pathTextBox.Location = new System.Drawing.Point(12, 193);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(232, 20);
+            this.pathTextBox.TabIndex = 3;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(250, 191);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 4;
+            this.browseButton.Text = "Browse...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 252);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Directory Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button browseButton;
+    }
+}
+
---
+++ DirectoryCreator/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace DirectoryCreator {
+
+    public partial class Form1 : Form {
+        string PATH = "";
+        private string[] pathParts;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e) {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            string regexedText = Regex.Replace(textBox.Text, "[^a-zA-Z0-9а-яА-Я ]", "");
+            string[] words = new string[regexedText.Split(' ').Length];
+            words = regexedText.Split(' ');
+            string commandLine = pathParts[0] + ":" + " & cd " + pathParts[1];
+            for (int i = 0; i < words.Length; i++) {
+                commandLine += " & " + "mkdir " + words[i] + " & " + "cd " + words[i];
+            }
+            psi.FileName = "cmd";
+            psi.Arguments = @"/c" + commandLine; // /c - zakrit'. k - ostavit'
+            Process.Start(psi);
+            label1.Text = "Catalogs has been created";
+            textBox.Text = "";
+            Console.WriteLine(pathParts[0] + ", " + pathParts[1]);
+        }
+
+        private void browseButton_Click(object sender, EventArgs e) {
+            DialogResult res = folderBrowserDialog1.ShowDialog();
+            if (res == DialogResult.OK) {
+                PATH = folderBrowserDialog1.SelectedPath;
+            }
+            pathTextBox.Text = PATH;
+            createButton.Enabled = true;
+            pathParts = PATH.Split(':'); //[0] - literal, [1] - put'
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e) {
+            if (textBox.Text == "" || PATH == "") {
+                createButton.Enabled = false;
+            } else {
+                createButton.Enabled = true;
+            }
+        }
+    }
+
+}
